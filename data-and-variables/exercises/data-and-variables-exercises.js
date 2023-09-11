eb308b099283c22b7368fba293fd8e69a7bfbf46
Quincy Nguyen <adambeckar123@gmail.com>
2023-09-10 22:36:16 -0500

    declare and assign variables

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,7 +1,15 @@
 // Declare and assign the variables below
-
+let spaceShuttle = 'Determination';
+let shuttleSpeed = 17500;
+let distancetoMars = 225000000;
+let distancetoMoon = 384400;
+let mph = 0.621;
 // Use console.log to print the 'typeof' each variable. Print one item per line.
-
+console.log(typeof spaceShuttle);
+console.log(typeof shuttleSpeed);
+console.log(typeof distancetoMars);
+console.log(typeof distancetoMoon);
+console.log(typeof mph);
 // Calculate a space mission below
 
 // Print the results of the space mission calculations below
