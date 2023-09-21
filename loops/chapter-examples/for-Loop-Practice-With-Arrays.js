e19afd08da864800c48e1cceedb39764b22e8069
Quincy Nguyen <adambeckar123@gmail.com>
2023-09-21 05:51:36 -0500

    Completed loops exercises

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,15 @@
 // create an array variable containing the names
-
+let family = ["Quincy", "Amy", "Tammy", "Alec", "Julius"]
 // write a for loop that prints each name on a different line
+for (let i = 0; i < family.length; i++) {
+    console.log(family[i]);
+} 
+
+let n = 6;
+let total = 0;
+
+for(let i = 1; i <= n; i++) {
+    total += i;
+}
+
+console.log(total);
--- loops/chapter-examples/for-Loop-Practice-With-Strings.js
+++ loops/chapter-examples/for-Loop-Practice-With-Strings.js
@@ -1,4 +1,14 @@
 // Create a string variable containing your name.
-
+let nickname = "Quincy"
 
 // Write a for loop that prints each character in your name on a different line.
+
+for (let i = 0; i < nickname.length; i++) {
+    console.log(nickname[i]);
+}
+
+let languages = ["JS", "Java", "C#", "Python"];
+
+for (let i = 0; i < languages.length; i++) {
+    console.log(languages[i]);
+}
