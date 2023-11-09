fc99af2aa11bf17b629401718bf7bc436384e52e
Quincy Nguyen <adambeckar123@gmail.com>
2023-11-09 05:28:21 -0600

    finished exercise

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -2,9 +2,24 @@ function init () {
     const missionAbort = document.getElementById("abortMission");
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
-
     // Put your code for the exercises here.
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+    });
     
+    missionAbort.addEventListener('click', event=> {
+        let response = window.confirm("Are you sure you want to abort the mission?")
+        if (response === true) {
+            window.alert("Mission aborted! Space shuttle returning home")
+        }
+    })
+}
+ 
+function changeBackground() {
+    var text = document.getElementById("abortMission").style.backgroundColor = "red";
 }
 
+function backToNormal() {
+    var text = document.getElementById("abortMission").style.backgroundColor = "";
+}
 window.addEventListener("load", init);
